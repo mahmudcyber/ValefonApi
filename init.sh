e5c8f3e9251d2a6422006c1e5830851add8269e8
mahmudcyber <56259535+mahmudcyber@users.noreply.github.com>
2025-09-21 11:07:08 +0100

    Commented  init.sh

--- init.sh
+++ init.sh
@@ -2,11 +2,11 @@
 set -e
 
 # Run migrations AFTER the app has env + Postgres
-php artisan config:clear
-php artisan route:clear
-php artisan cache:clear
+##php artisan config:clear
+##php artisan route:clear
+##php artisan cache:clear
 
 # Run scripts that were skipped during composer install
-composer run-script post-autoload-dump --no-dev --optimize-autoloader
+##composer run-script post-autoload-dump --no-dev --optimize-autoloader
 
-php artisan migrate --force
+##php artisan migrate --force
